57e3937447c63c1a6fa3ccbb691de5d1a5a6be5f
rafimd422 <rafimf2222@gmail.com>
2023-11-08 14:48:42 +0600

    complated a delete api to delete data on client side without any hassle

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sopxnju.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -22,31 +22,37 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+     await client.connect();
 
     const database = client.db("FoodDB");
     const foodCollection = database.collection("foods");
 
     app.get("/foods", async (req, res) => {
-      const cursor = foodCollection.find();
+      let query = {};
+    
+      if (req.query.email) {
+        query = { email: req.query.email };
+      }
+    
+      const cursor = foodCollection.find(query);
       const result = await cursor.toArray();
       res.send(result);
     });
-
-    app.get("/food", async (req, res) => {
-      let query = {};
-      if (req.query?.email) {
-        query = { email: req.query.email };
-      }
-      const result = await foodCollection.find(query).toArray();
-      res.send(result);
-    });
+    
 
     app.post("/foods", async (req, res) => {
       const food = req.body;
       const result = await foodCollection.insertOne(food);
       res.send(result);
     });
+
+    app.delete('/foods/:id',async(req,res) => {
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      console.log(query)
+      const result = await foodCollection.deleteOne(query)
+      res.send(result)
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
