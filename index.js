6ae854422a16d8787e028dd2bd4116ee6fe90c3e
rafimd422 <rafimf2222@gmail.com>
2023-11-09 17:37:41 +0600

    added some configaration to deploy our server side on vercel

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-     await client.connect();
+    // await client.connect();
 
     const database = client.db("FoodDB");
     const foodCollection = database.collection("foods");
@@ -37,14 +37,14 @@ async function run() {
       const result = await cursor.toArray();
       res.send(result);
     });
-    
-    app.get('/foods/:id', async (req, res) => {
-      const id = req.params.id
-      const Query = {_id: new ObjectId(id)}
-      console.log(Query)
-      const result = await foodCollection.findOne(Query)
-      res.send(result)
-    })
+
+    app.get("/foods/:id", async (req, res) => {
+      const id = req.params.id;
+      const Query = { _id: new ObjectId(id) };
+      console.log(Query);
+      const result = await foodCollection.findOne(Query);
+      res.send(result);
+    });
 
     app.post("/foods", async (req, res) => {
       const food = req.body;
@@ -52,93 +52,90 @@ async function run() {
       res.send(result);
     });
 
-
-    app.put('/foods/:id', async(req,res) => {
-      const id = req.params.id
+    app.put("/foods/:id", async (req, res) => {
+      const id = req.params.id;
       const foods = req.body;
-      const filter = {_id: new ObjectId(id)}
+      const filter = { _id: new ObjectId(id) };
       const options = { upsert: true };
-      const updateFood = { $set: {
+      const updateFood = {
+        $set: {
           foodName: foods.foodName,
           foodImg: foods.foodImg,
-          status:foods.status,
-          quantity:foods.quantity,
+          status: foods.status,
+          quantity: foods.quantity,
           pickUplocation: foods.pickUplocation,
-          expireDate:foods.expireDate,
-          note: foods.note
-        }
-      }
+          expireDate: foods.expireDate,
+          note: foods.note,
+        },
+      };
       try {
-        const result = await foodCollection.updateOne(filter,updateFood, options);
+        const result = await foodCollection.updateOne(
+          filter,
+          updateFood,
+          options
+        );
         res.send(result);
       } catch (error) {
         console.error(error);
-        res.status(500).send('An error occurred while updating the document.');
+        res.status(500).send("An error occurred while updating the document.");
       }
-    })
+    });
 
-    app.delete('/foods/:id',async(req,res) => {
+    app.delete("/foods/:id", async (req, res) => {
       const id = req.params.id;
-      const query = {_id: new ObjectId(id)}
-      console.log(query)
-      const result = await foodCollection.deleteOne(query)
-      res.send(result)
-    })
+      const query = { _id: new ObjectId(id) };
+      console.log(query);
+      const result = await foodCollection.deleteOne(query);
+      res.send(result);
+    });
 
+    // apis for requested foods Collections
 
-// apis for requested foods Collections 
+    app.get("/reqfoods", async (req, res) => {
+      let query = {};
+      if (req.query?.email) {
+        query = { requesterEmail: req.query.email }; // Use requesterEmail here
+      }
+      const cursor = reqFoodCollection.find(query);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
 
+    app.get("/reqfoods/:id", async (req, res) => {
+      const id = req.params.id;
+      const Query = { id: id };
+      console.log(Query);
+      const result = await reqFoodCollection.findOne(Query);
+      res.send(result);
+    });
 
+    app.post("/reqfoods", async (req, res) => {
+      const food = req.body;
+      const result = await reqFoodCollection.insertOne(food);
+      res.send(result);
+    });
 
-app.get("/reqfoods", async (req, res) => {
-  let query = {};
-  if (req.query?.email) {
-    query = { requesterEmail: req.query.email }; // Use requesterEmail here
-  }
-  const cursor = reqFoodCollection.find(query);
-  const result = await cursor.toArray();
-  res.send(result);
-});
+    app.patch("/reqfoods/:id", async (req, res) => {
+      const id = req.params.id;
+      const filter = { _id: new ObjectId(id) };
+      console.log(id, filter);
+      const reqFoods = req.body;
+      console.log("status", reqFoods);
+      const updateDoc = {
+        $set: {
+          status: reqFoods.status,
+        },
+      };
+      const result = await reqFoodCollection.updateOne(filter, updateDoc);
+      res.send(result);
+    });
 
-
-app.get('/reqfoods/:id', async (req, res) => {
-  const id = req.params.id
-  const Query = {id: id}
-  console.log(Query)
-  const result = await reqFoodCollection.findOne(Query)
-  res.send(result)
-})
-
-app.post("/reqfoods", async (req, res) => {
-  const food = req.body;
-  const result = await reqFoodCollection.insertOne(food);
-  res.send(result);
-});
-
-
-app.patch('/reqfoods/:id', async (req, res) => {
-  const id = req.params.id
-  const filter = {_id: new ObjectId(id)}
-  console.log(id, filter)
-  const reqFoods = req.body;
-  console.log('status', reqFoods)
-  const updateDoc = {
-    $set: {
-      status: reqFoods.status
-    }
-  }
-  const result = await reqFoodCollection.updateOne(filter, updateDoc)
-  res.send(result)
-})
-
-
-app.delete('/reqfoods/:id',async(req,res) => {
-  const id = req.params.id;
-  const query = {_id: new ObjectId(id)}
-  const result = await reqFoodCollection.deleteOne(query)
-  res.send(result)
-})
-
+    app.delete("/reqfoods/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await reqFoodCollection.deleteOne(query);
+      res.send(result);
+    });
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
